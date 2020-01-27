648234424b705bb1668fed41cb92b163d01e9589
Aseel Maqnassah <54496563+aseelmqannasa@users.noreply.github.com>
2020-01-27 21:10:20 +0200

    Add files via upload

--- HandValidator.rb
+++ HandValidator.rb
@@ -11,171 +11,37 @@ group = user_input.split(/,/)
 $group_suits=Set.new
 $group_ranks=Array.new
 
-def CheckJokers(group)
-  if(group.include?'1j')
-      if($group_suits.size == 1)
-        if(group.index('1j') == 0 or group.index('1j') == group.size-1) #the joker is at first or last index
-          if(group[group.index('1j')+1].include?'A' or group[group.index('1j')-1].include?'A')
-            return false
-          else
-            group.delete('1j')
-            CheckSequence(group)
-          end
-        else #joker is at the middle
-          if(group[group.index('1j')+1].split("")[0] == 'A')
-            if($ranks.index(group[group.index('1j')-1].split("")[0]) == 1 or $ranks.index(group[group.index('1j')-1].split("")[0]) == 10)
-              group.delete('1j')
-              if(group.size > 2)
-                if(CheckSequence(group[0..group.index('1j')-1]))
-                  CheckSequence(group[group.index('1j')+1..group.size-1])
-                else
-                  return false
-                end
-              else
-                return true
-              end
-            else
-              return false
-            end
-          elsif (group[group.index('1j')-1].split("")[0] == 'A')
-            if($ranks.index(group[group.index('1j')+1].split("")[0]) == 1 or $ranks.index(group[group.index('1j')+1].split("")[0]) == 10)
-              if(group.size > 2)
-                if(CheckSequence(group[0..group.index('1j')-1]))
-                  CheckSequence(group[group.index('1j')+1..group.size-1])
-                  group.delete('1j')
-                else
-                  return false
-                end
-              else
-                return true
-              end
-              #CheckSequence if group size is bigger than 2 else just return true
-            else
-              return false
-            end
-        else
-          if(($ranks.index(group[group.index('1j')+1].split("")[0]) - $ranks.index(group[group.index('1j')-1].split("")[0])).abs == 2)
-            if(group.size > 3)
-              if(CheckSequence(group[0..group.index('1j')-1]))
-                CheckSequence(group[group.index('1j')+1..group.size-1])
-                group.delete('1j')
-              else
-                return false
-              end
-            else
-              return true
-            end
-            else
-              return false
-            end
-        end
-      end
-    else if($group_suits.size >= 2 and group.size < 5)
-        group.delete('1j')
-        CheckSameRank(group)
+def SplitGroup(group)
+  $group_suits.clear()
+  $group_ranks.clear()
+  group.each do |card|
+    if (!card.include?'1j' and !card.include?'2j')
+      if(card.length == 2)
+        $group_ranks << card.split('')[0]
+        $group_suits << card.split('')[1]
       else
-        return false
+        $group_ranks << card.split('')[0] + card.split('')[1]
+        $group_suits << card.split('')[2]
       end
-    end
-  elsif(group.include?'2j')
-    if($group_suits.size == 1)
-      if(group.index('2j') == 0 or group.index('2j') == group.size-1) #the joker is at first or last index
-        if(group[group.index('2j')+1].include?'A' or group[group.index('2j')-1].include?'A')
-          return false
-        else
-          group.delete('2j')
-          CheckSequence(group)
-        end
-      else #joker is at the middle
-        if(group[group.index('2j')+1].split("")[0] == 'A')
-          if($ranks.index(group[group.index('2j')-1].split("")[0]) == 1 or $ranks.index(group[group.index('2j')-1].split("")[0]) == 10)
-            if(group.size > 2)
-              if(CheckSequence(group[0..group.index('2j')-1]))
-                CheckSequence(group[group.index('2j')+1..group.size-1])
-                group.delete('2j')
-              else
-                return false
-              end
-            else
-              return true
-            end
-            #CheckSequence if group size is bigger than 2 else just return true
-          else
-            return false
-          end
-        elsif (group[group.index('2j')-1].split("")[0] == 'A')
-          if($ranks.index(group[group.index('2j')+1].split("")[0]) == 1 or $ranks.index(group[group.index('2j')+1].split("")[0]) == 10)
-            group.delete('2j')
-            if(group.size > 2)
-              if(CheckSequence(group[0..group.index('2j')-1]))
-                CheckSequence(group[group.index('2j')+1..group.size-1])
-              else
-                return false
-              end
-            else
-              return true
-            end
-            #CheckSequence if group size is bigger than 2 else just return true
-          else
-            return false
-          end
-      else
-        if(($ranks.index(group[group.index('2j')+1].split("")[0]) - $ranks.index(group[group.index('2j')-1].split("")[0])).abs == 2)
-            if(CheckSequence(group[0..group.index('2j')-1]))
-              CheckSequence(group[group.index('2j')+1..group.size-1])
-            else
-              return false
-            end
-          else
-            return false
-        end
-      end
-    end
-    elsif($group_suits.size > 1 and group.size < 5)
-      group.delete('2j')
-      CheckSameRank(group)
     end
   end
 end
 
-def CheckAces(group)
-  if($group_ranks.index('A')==0)
-    if(($ranks.index('A').to_i - $ranks.index($group_ranks[1]).to_i).abs == 12 or ($ranks.index('A').to_i - $ranks.index($group_ranks[1]).to_i).abs == 1)
-      $group_ranks.delete('A')
-        if(CheckSequence($group_ranks))
-          return true
-        else
-          return false
-        end
-    else
-      return false
-    end
-  else if ($group_ranks.index('A')==$group_ranks.length-1)
-    if(($ranks.index('A').to_i - $ranks.index($group_ranks[$group_ranks.index('A')-1]).to_i).abs == 12 or ($ranks.index('A').to_i - $ranks.index($group_ranks[$group_ranks.index('A')-1]).to_i).abs == 1)
-      $group_ranks.delete('A')
-        if(CheckSequence($group_ranks))
-          return true
-        else
-          return false
-        end
-    else
-      return false
-    end
-  else
-    return false
-  end
-end
-end
-
-def CheckSequence(group)
+def SequentialGroup(group)
   if(group.size == 1)
     return true
-  else
-  if ($group_ranks.include?'A')
-    if(CheckAces(group))
-      return true
+  end
+  if($group_ranks.index('A')==0)
+    if(($ranks.index('A').to_i - $ranks.index($group_ranks[1]).to_i).abs != 12 and ($ranks.index('A').to_i - $ranks.index($group_ranks[1]).to_i).abs != 1)
+      return false
     end
-    else
+  elsif ($group_ranks.index('A')==$group_ranks.size-1)
+    if(($ranks.index('A').to_i - $ranks.index($group_ranks[$group_ranks.index('A')-1]).to_i).abs != 12 and ($ranks.index('A').to_i - $ranks.index($group_ranks[$group_ranks.index('A')-1]).to_i).abs != 1)
+      return false
+    end
+  end
+  $group_ranks.delete('A')
+
   i = -1
   loop do
     i +=1
@@ -188,80 +54,195 @@ def CheckSequence(group)
   end
   return true
 end
-end
+
+#Sequence
+def CheckSequence(group)
+  SplitGroup(group)
+  if($group_suits.size > 1) #must be one suit to accomplish a sequential group
+    return false
+  end
+  if(group.include?'1j')
+    if(group.index('1j') == 0 or group.index('1j') == group.size-1) #the joker is at first or last index
+      if(group.index('1j') == 0)
+        if(group[group.index('1j')+1].include?'A')
+          return false
+        end
+      end
+        if(group.index('1j') == group.size-1)
+          if(group[group.index('1j')-1].include?'A')
+            return false
+          end
+        end
+        if(!SequentialGroup($group_ranks))
+          return false
+        end
+      return true
+    else #the joker is at the middle
+      if(group[group.index('1j')+1].include?'A')
+        if($ranks.index(group[group.index('1j')-1].split("")[0]) != 1 and $ranks.index(group[group.index('1j')-1].split("")[0]) != 10)
+          return false
+        end
+        if(group.size > 3)
+          if(!SequentialGroup(group[0..group.index('1j')-1]))
+            return false
+          end
+          if(!SequentialGroup(group[group.index('1j')+1..group.size-1]))
+            return false
+          end
+        end
+      elsif(group[group.index('1j')-1].include?'A')
+        if($ranks.index(group[group.index('1j')+1].split("")[0]) != 1 and $ranks.index(group[group.index('1j')+1].split("")[0]) != 10)
+          return false
+        end
+        if(group.size > 3)
+          SplitGroup(group[0..group.index('1j')-1])
+          if(!SequentialGroup(group[0..group.index('1j')-1]))
+            return false
+          end
+          SplitGroup(group[group.index('1j')+1..group.size-1])
+          if(!SequentialGroup(group[group.index('1j')+1..group.size-1]))
+            return false
+          end
+        end
+        return true
+      else
+        if(($ranks.index(group[group.index('1j')+1].split("")[0]) - $ranks.index(group[group.index('1j')-1].split("")[0])).abs != 2)
+          return false
+        end
+          if(group.size > 3)
+            SplitGroup(group[0..group.index('1j')-1])
+            if(!SequentialGroup(group[0..group.index('1j')-1]))
+              return false
+            end
+            SplitGroup(group[group.index('1j')+1..group.size-1])
+            if(!SequentialGroup(group[group.index('1j')+1..group.size-1]))
+              return false
+            end
+          end
+          return true
+        end
+      end
+    elsif(group.include?'2j')
+      if(group.index('2j') == 0 or group.index('2j') == group.size-1) #the joker is at first or last index
+        if(group.index('2j') == 0)
+          if(group[group.index('2j')+1].include?'A')
+            return false
+          end
+        end
+          if(group.index('2j') == group.size-1)
+            if(group[group.index('2j')-1].include?'A')
+              return false
+            end
+          end
+          if(!SequentialGroup($group_ranks))
+            return false
+          end
+        return true
+      else #the joker is at the middle
+        if(group[group.index('2j')+1].include?'A')
+          if($ranks.index(group[group.index('2j')-1].split("")[0]) != 1 and $ranks.index(group[group.index('2j')-1].split("")[0]) != 10)
+            return false
+          end
+          if(group.size > 3)
+            if(!SequentialGroup(group[0..group.index('2j')-1]))
+              return false
+            end
+            if(!SequentialGroup(group[group.index('2j')+1..group.size-1]))
+              return false
+            end
+          end
+        elsif(group[group.index('2j')-1].include?'A')
+          if($ranks.index(group[group.index('2j')+1].split("")[0]) != 1 and $ranks.index(group[group.index('2j')+1].split("")[0]) != 10)
+            return false
+          end
+          if(group.size > 3)
+            SplitGroup(group[0..group.index('2j')-1])
+            if(!SequentialGroup(group[0..group.index('2j')-1]))
+              return false
+            end
+            SplitGroup(group[group.index('2j')+1..group.size-1])
+            if(!SequentialGroup(group[group.index('2j')+1..group.size-1]))
+              return false
+            end
+          end
+          return true
+        else
+          if(($ranks.index(group[group.index('2j')+1].split("")[0]) - $ranks.index(group[group.index('2j')-1].split("")[0])).abs != 2)
+            return false
+          end
+            if(group.size > 3)
+              SplitGroup(group[0..group.index('2j')-1])
+              if(!SequentialGroup(group[0..group.index('2j')-1]))
+                return false
+              end
+              SplitGroup(group[group.index('2j')+1..group.size-1])
+              if(!SequentialGroup(group[group.index('2j')+1..group.size-1]))
+                return false
+              end
+            end
+            return true
+          end
+        end
+      else
+        if(!SequentialGroup(group))
+          return false
+        end
+        return true
+      end
+    end
+
+#Validate the group of cards
+def GeneralValidation(group)
+#check length
+  if (group.size <= 2 or group.size > 13)
+    return false
+  end
+#check jokers
+  if (group.include?'1j' and group.include?'2j')
+    return false
+  end
+  return true
 end
 
+#cards of the same rank
 def CheckSameRank(group)
-  group_ranks_set=Set.new
+  SplitGroup(group)
+  group_ranks_set=Set.new #must be only one rank
   group.each do |card|
     if (!card.include?'j')
     group_ranks_set << card.split('')[0]
     end
   end
 
+#delete jokers
+  if(group.include?'1j')
+    group.delete('1j')
+  end
+  if(group.include?'2j')
+    group.delete('2j')
+  end
+
+#validation
   if($group_suits.size==group.size)
     if(group_ranks_set.size==1)
       return true
-    else
-      return false
     end
-  else
-    return false
   end
+  return false
 end
 
-def CheckLength(group)
-  if (group.size <= 2 or group.size > 5)
-    return false
-  end
-  return true
-end
-
+# main function
 def validgroup(group)
-  if(CheckLength(group))
-    group.each do |card|
-      if (!card.include?'1j' and !card.include?'2j')
-        if(card.length == 2)
-          $group_ranks << card.split('')[0]
-          $group_suits << card.split('')[1]
-        else
-          $group_ranks << card.split('')[0] + card.split('')[1]
-          $group_suits << card.split('')[2]
-        end
-      end
-    end
-    if($group_suits.size==1)
-      if (group.include?'1j' or group.include?'2j')
-        if(CheckJokers(group))
-          return true
-        else
-          return false
-        end
-      elsif ($group_ranks.include?'A')
-        if(CheckAces(group))
-          return true
-        else
-          return false
-        end
-    else
-      CheckSequence(group)
-    end
-  else
-    if($group_suits.size>=2)
-      if (group.include?'1j' or group.include?'2j')
-        if(CheckJokers(group))
-          return true
-        else
-          return false
-        end
-      else
-        return CheckSameRank(group)
-      end
-    end
-    end
-  else
+  if(!GeneralValidation(group))
     return false
   end
+  if(CheckSequence(group))
+    return true
+  end
+  if(CheckSameRank(group))
+    return true
+  end
+  return false
 end
 
 puts validgroup(group)
